c684ce994e5e2cc81700a7ff1a574dcb59410a18
richharv557 <richharv557@gmail.com>
2022-05-09 19:19:15 -0500

    refactored to make more readable.

--- caesar.rb
+++ caesar.rb
@@ -3,21 +3,17 @@ def caesar_cipher(string,shift_factor)
     character = character.ord
     if character >= 97 && character <= 122
       new_ord = character + shift_factor
-      if new_ord > 122
-        new_ord -= 26
-      end
+      new_ord -= 26 if new_ord > 122
       new_ord
     elsif character >= 65 && character <= 90
       new_ord = character + shift_factor
-      if new_ord > 90
-        new_ord -= 26
-      end
+      new_ord -= 26 if new_ord > 90
       new_ord
     else
       character
     end
   end
-  new_array.map { |item| item == nil ? ' ' : item.chr }.join
+  puts new_array.map { |item| item == nil ? ' ' : item.chr }.join
 end
 
-p caesar_cipher("What a string!", 5)
+caesar_cipher("What a string!", 5)
